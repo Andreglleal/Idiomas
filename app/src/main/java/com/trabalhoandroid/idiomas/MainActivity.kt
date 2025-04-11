e393072cefd7486036a144fba6b5f463ac26c307
Andreglleal <andre.lemeslea@gmail.com>
2025-04-11 09:53:01 -0300

    termino do layout

--- app/src/main/java/com/trabalhoandroid/idiomas/MainActivity.kt
+++ app/src/main/java/com/trabalhoandroid/idiomas/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -32,8 +33,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.trabalhoandroid.idiomas.ui.theme.Blue
 import com.trabalhoandroid.idiomas.ui.theme.White
 
@@ -140,11 +144,20 @@ fun Home() {
             Image(
                 painter = painterResource(R.drawable.bandeira1),
                 contentDescription = "bandeira do brasil",
-                modifier = Modifier.size(300.dp)
+                modifier = Modifier.size(250.dp)
             )
             Text(
-                text = "Brasil",
-                modifier = Modifier.padding(100.dp)
+                text = stringResource(R.string.idioma),
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = stringResource(R.string.descricao),
+                fontSize = 18.sp,
+                modifier = Modifier
+                    .padding(20.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .border(width = 2.dp, color = Blue).padding(10.dp)
             )
         }
     }
